3bc34a9ae3ac403949761345309d56fbf2807124
Keith Owens Jr. <owenskei000@warren.k12.in.us>
2024-09-27 11:53:19 -0400

    seriously for reals this time

---
+++ src/main.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val Keith = Person("Keith", 17, "Bowling", null)
+    val Xavier = Person("Xavier", 17, "Build nuclear weapons", Keith)
+
+    Keith.showProfile()
+    Xavier.showProfile()
+}
+
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        println("Name: $name")
+        println("Age: $age")
+        if(hobby != null) {
+            print("Likes to $hobby. ")
+        }
+        if(referrer != null) {
+            print("Has a referrer named ${referrer.name}")
+            if(referrer.hobby != null) {
+                print(", who likes to ${referrer.hobby}.")
+            } else {
+                print(".")
+            }
+        } else {
+            print("Doesn't have a referrer.")
+        }
+        print("\n\n")
+    }
+}
